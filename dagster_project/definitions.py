858f8707adddc7cb64dbe1b82c55950f7c521db0
khaibom <tdkhai.work@gmail.com>
2025-11-20 00:58:08 +0100

    add schedule, definition, job for lstm asset

--- dagster_project/definitions.py
+++ dagster_project/definitions.py
@@ -1,12 +1,22 @@
 from dagster import Definitions, load_assets_from_modules
 
-from .assets import raw_daily_data, lag_features, indicator_features, targets, preprocessing
-from .jobs import job_weekday_0800, job_weekday_0810
-from .schedules import schedule_weekday_0800, schedule_weekday_0810
+from .assets import raw_daily_data, lag_features, indicator_features, targets, preprocessing, lstm_model
+from .jobs import (
+    job_get_data,
+    job_lstm_predict_reg,
+    job_lstm_predict_cls,
+    job_lstm_full
+)
+from .schedules import schedule_weekday_0800
 
-all_assets = load_assets_from_modules([raw_daily_data, lag_features, indicator_features, targets, preprocessing])
-all_jobs = [job_weekday_0800, job_weekday_0810]
-all_schedules = [schedule_weekday_0800, schedule_weekday_0810]
+all_assets = load_assets_from_modules([raw_daily_data, lag_features, indicator_features, targets, preprocessing, lstm_model])
+all_jobs = [
+    job_get_data,
+    job_lstm_predict_reg,
+    job_lstm_predict_cls,
+    job_lstm_full
+]
+all_schedules = [schedule_weekday_0800]
 
 defs = Definitions(
     assets=all_assets,
--- dagster_project/jobs.py
+++ dagster_project/jobs.py
@@ -1,13 +1,32 @@
 from dagster import define_asset_job
 
 selection = ['asset_market_raw', 'asset_features_lagged', 'asset_features_full', 'target_price', 'target_updown', 'asset_preprocessed_data']
-job_weekday_0800 = define_asset_job(
+job_get_data = define_asset_job(
     name='get_data_job',
     selection=selection,
 )
 
-selection = ['asset_preprocessed_data']
-job_weekday_0810 = define_asset_job(
-    name='prepare_data_for_LSTM',
-    selection=selection,
+# LSTM REGRESSION jobs
+job_lstm_predict_reg = define_asset_job(
+    name='lstm_predict_pipeline_reg',
+    selection=['asset_market_raw', 'asset_features_lagged', 'asset_features_full',
+               'target_price', 'target_updown', 'asset_preprocessed_data',
+               'lstm_trained_model_reg', 'lstm_predictions_reg'],
+)
+
+# LSTM CLASSIFICATION jobs
+job_lstm_predict_cls = define_asset_job(
+    name='lstm_predict_pipeline_cls',
+    selection=['asset_market_raw', 'asset_features_lagged', 'asset_features_full',
+               'target_price', 'target_updown', 'asset_preprocessed_data',
+               'lstm_trained_model_cls', 'lstm_predictions_cls'],
+)
+
+# COMBINED job - Train and predict both regression and classification
+job_lstm_full = define_asset_job(
+    name='lstm_full_pipeline',
+    selection=['asset_market_raw', 'asset_features_lagged', 'asset_features_full', 
+               'target_price', 'target_updown', 'asset_preprocessed_data', 
+               'lstm_trained_model_reg', 'lstm_predictions_reg',
+               'lstm_trained_model_cls', 'lstm_predictions_cls'],
 )
